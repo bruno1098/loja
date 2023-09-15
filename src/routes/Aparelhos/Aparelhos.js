f53a5fc01e579f170b592ed59b092c307a963c4b
bruno1098 <rm98470@fiap.com.br>
2023-09-15 20:48:44 -0300

    criando um novo valor na lista dos aparalhos

--- src/routes/Aparelhos/Aparelhos.js
+++ src/routes/Aparelhos/Aparelhos.js
@@ -12,43 +12,49 @@ const aparelhosData = [
       id: 1,
       nome: "iPad",
       descricao: "O iPad é uma linha de tablets da Apple, conhecida por seu design elegante, desempenho confiável.",
-      preco: 999.99,
+      preco: "Por apenas R$4.554,44 e em até 10x R$ 455,48 sem juros!!",
       imagem: ImagemIpad,
+      detalhes: "Detalhes"
     },
     {
       id: 2,
       nome: "NoteBook",
       descricao: "Este notebook é um computador portátil compacto, adequado para tarefas cotidianas, como navegação na web, criação de documentos e reprodução de mídia.",
-      preco: 799.99,
+      preco: "Por apenas R$2.554,44 e em até 10x R$ 255,44 sem juros!!",
       imagem: ImagemNotebook,
+      detalhes: "Detalhes"
     },
     {
       id: 3,
       nome: "Apple Watch Series 3",
       descricao: "O Apple Watch é um smartwatch da Apple, oferecendo recursos de monitoramento de saúde, notificações e integração com dispositivos Apple.",
-      preco: 499.99,
+      preco: "Por apenas R$5.554,44 e em até 10x R$ 554,48 sem juros!!",
       imagem: ImagemApplewatch,
+      detalhes: "Detalhes"
     },
     {
       id: 4,
       nome: "MacBook Pro iMac",
       descricao: "O iMac é um computador all-in-one da Apple, reconhecido pelo seu design elegante e compacto.",
-      preco: 699.99,
+      preco: "Por apenas R$22.554,44 e em até 10x R$ 2.554,48 sem juros!!",
       imagem: ImagemMac,
+      detalhes: "Detalhes"
     },
     {
       id: 5,
       nome: "Air Pods",
       descricao: "Os AirPods da Apple são fones de ouvido sem fio que oferecem uma experiência de áudio conveniente e de alta qualidade.",
-      preco: 399.99,
+      preco: "Por apenas R$1.300,00 e em até 10x R$ 130,00 sem juros!!",
       imagem: ImagemAirpods,
+      detalhes: "Detalhes"
     },
     {
         id: 6,
         nome: "Pc Gamer",
         descricao: "Um PC gamer é um computador especialmente projetado para jogos de alta performance.",
-        preco: 399.99,
+        preco: "Por apenas R$3.200,00 e em até 10x R$ 320,00 sem juros!!",
         imagem: ImagemPc,
+        detalhes: "Detalhes"
       },
   ];
   

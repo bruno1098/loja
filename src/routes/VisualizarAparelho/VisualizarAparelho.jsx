aa7a2f3225423ed9c627bdc10129e18e12fdb5e2
bruno1098 <rm98470@fiap.com.br>
2023-09-15 20:49:51 -0300

    Corrigindo o VisualizarAparelho para aparecer correto na página

--- src/routes/VisualizarAparelho/VisualizarAparelho.jsx
+++ src/routes/VisualizarAparelho/VisualizarAparelho.jsx
@@ -1,21 +1,16 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import aparelhosData from "../Aparelhos/Aparelhos";
 
 export default function VisualizarAparelho(){
-    const { id } = useParams();
-    const aparelho = aparelhosData.find((ap) => ap.id === parseInt(id));
-  
-    if (!aparelho) {
-      return <div>Aparelho não encontrado.</div>;
-    }
-  
-    return (
-      <div>
-        <h2>{aparelho.nome}</h2>
-        <img src={aparelho.imagem} alt={aparelho.nome} />
-        <p>Descrição: {aparelho.descricao}</p>
-        <p>Preço: R$ {aparelho.preco}</p>
-      </div>
-    );
+    const {id} = useParams();
+    const aparelho = aparelhosData.find((aparelhos) => aparelhos.id == parseInt(id));
+
+return(
+    <div className="VerAparelho">
+      <h1>{aparelho.nome}</h1>
+      <h2>{aparelho.descricao}</h2>
+      <p>{aparelho.preco}</p>
+      <img src={`${aparelho.imagem}`} alt={`${aparelho.descricao}`} />
+    </div>
+)
   }
